ed7a3c8f3601fe7615ba2939667464332a1a16da
Laszlo Heves <heves.laszlo@gmail.com>
2025-07-13 21:18:58 +0000

    fix: __dirname module scope

--- .github/workflows/best-before-github-action.js
+++ .github/workflows/best-before-github-action.js
@@ -1,5 +1,9 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const bestBeforeDocComment = process.env.BEST_BEFORE_COMMENT || 'best-before-doc';
 
