b07217d64d2d018239c1b3853e9edfbee258c2c5
Laszlo Heves <heves.laszlo@gmail.com>
2025-07-13 20:43:17 +0000

    feat: print md files

--- .github/workflows/best-before-github-action.js
+++ .github/workflows/best-before-github-action.js
@@ -1,1 +1,15 @@
-console.log("Hello, world!");
+const fs = require('fs');
+const path = require('path');
+
+function logMarkdownFiles(dir) {
+    fs.readdirSync(dir, { withFileTypes: true }).forEach((entry) => {
+        const fullPath = path.join(dir, entry.name);
+        if (entry.isDirectory()) {
+            logMarkdownFiles(fullPath);
+        } else if (entry.isFile() && entry.name.endsWith('.md')) {
+            console.log(fullPath);
+        }
+    });
+}
+
+logMarkdownFiles(path.resolve(__dirname, '../../..'));
